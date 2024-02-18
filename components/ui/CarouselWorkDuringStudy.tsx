3d86350641930842bbf3e94b3d87b0ea26b505ea
Supawan <dmdmmm@Supawans-MacBook-Air.local>
2024-02-18 14:11:50 +0700

    14.11 18/02/2567 commit

--- components/ui/CarouselWorkDuringStudy.tsx
+++ components/ui/CarouselWorkDuringStudy.tsx
@@ -4,7 +4,7 @@ import { dataWorkDuring1, dataWorkDuring2, dataWorkDuring3 } from "../data/dataW
 
 const CarouselWorkDuringStudy = () => {
     return (
-        <Carousel className="text-white py-8">
+        <div className="text-white py-8">
             <div className="carousel-grid gap-5 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 lg:px-20 m-auto  w-full">
                 {dataWorkDuring1.map((items) => (
                     <ImageWorkDS key={items.id} image={items} />
@@ -20,7 +20,7 @@ const CarouselWorkDuringStudy = () => {
                     <ImageWorkDS key={items.id} image={items} />
                 ))}
             </div>
-        </Carousel>
+        </div>
     )
 }
 
