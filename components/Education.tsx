4e801313b9fd6a7778632dc71a59fce3bab415dc
Supawan <dmdmmm@Supawans-MacBook-Air.local>
2024-02-18 21:21:32 +0700

    21.21 18/02/2567 commit

--- components/Education.tsx
+++ components/Education.tsx
@@ -1,22 +1,19 @@
 import ResumeCard from "./ui/ResumeCard";
+import Link from "next/link";
 
 const Education = () => {
-
-    const handleDownloadPDF = () => {
-        const pdfUrl = 'pdf/transcript.pdf';
-        const a = document.createElement('a');
-        a.href = pdfUrl;
-        a.download = 'transcript.pdf';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-    };
 
     return (
         <div>
             <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4 lg:px-11">
                 <h2 className="text-xl md:text-3xl font-bold">ประวัติการศึกษา</h2>
-                <p className="z-[1] mt-[-5px] cursor-pointer text-sm text-orange-500 tracking-[4px] hover:text-white hover:bg-orange-500 hover:p-3 hover:text-center rounded-md hover:shadow-lg max-w-[240px] text-center duration-300" onClick={handleDownloadPDF}>Download Transcript</p>
+                <div className="z-[1] mt-[-5px] cursor-pointer text-sm text-orange-500 tracking-[4px] hover:text-white hover:bg-orange-500 hover:p-3 hover:text-center rounded-md hover:shadow-lg max-w-[240px] text-center duration-300">
+                    <Link href="https://firebasestorage.googleapis.com/v0/b/project-nextpos-828f7.appspot.com/o/transcript.pdf?alt=media&token=ff334dcd-2d4f-45c3-bdc2-0c391407273e" target="_blank">
+                        <p>
+                            Download Transcript
+                        </p>
+                    </Link>
+                </div>
             </div>
             <div className="w-full xl:border-l-[6px] border-l-black border-opacity-30 lg:flex lg:flex-col gap-4 lg:gap-10 lg:max-w-[965px] lgl:w-[965px]">
                 <ResumeCard
