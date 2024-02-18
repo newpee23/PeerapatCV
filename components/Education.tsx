0f47ea1e79bbb722d2e0a9efbf4d1700a61a1c6f
Supawan <dmdmmm@Supawans-MacBook-Air.local>
2024-02-18 21:16:25 +0700

    21.16 18/02/2567 commit

--- components/Education.tsx
+++ components/Education.tsx
@@ -18,7 +18,7 @@ const Education = () => {
                 <h2 className="text-xl md:text-3xl font-bold">ประวัติการศึกษา</h2>
                 <p className="z-[1] mt-[-5px] cursor-pointer text-sm text-orange-500 tracking-[4px] hover:text-white hover:bg-orange-500 hover:p-3 hover:text-center rounded-md hover:shadow-lg max-w-[240px] text-center duration-300" onClick={handleDownloadPDF}>Download Transcript</p>
             </div>
-            <div className="w-full lg:border-l-[6px] border-l-black border-opacity-30 lg:flex lg:flex-col gap-4 lg:gap-10 lg:max-w-[965px] lgl:w-[965px]">
+            <div className="w-full xl:border-l-[6px] border-l-black border-opacity-30 lg:flex lg:flex-col gap-4 lg:gap-10 lg:max-w-[965px] lgl:w-[965px]">
                 <ResumeCard
                     title="มหาวิทยาลัยพะเยา"
                     subTitle="University of Phayao"
